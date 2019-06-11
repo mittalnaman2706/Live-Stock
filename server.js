999592785d772ac3d02e1705dcf9be9ab764efd8
Naman Mittal <mittalnaman2706@gmail.com>
2019-06-11 12:12:59 +0530

    New Additions

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
-var http = require('http');
+var request = require('request');
+var http = require('https');
 var express=require("express");
+var path = require('path');
+var bodyParser=require('body-parser');
 
-var port = 2713;
+var port = 3000;
 var app = express();
 
+var apiKey = "JCONAHM5CV7PI1CS";
 
-app.get('/', function(req,res){
+var staticPath = path.join(__dirname, 'client');
+console.log(staticPath);
+app.use(express.static(staticPath));
 
-	res.sendFile(__dirname + '/main.html');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+app.get('/history', function(req,res){
+
+	var symbol = req.query.body;
+	console.log(symbol);
+	var AlphaVantageAPI = require('alpha-vantage-cli').AlphaVantageAPI;
+	var alphaVantageAPI = new AlphaVantageAPI(apiKey, 'compact', true);
+
+	alphaVantageAPI.getDailyData('GOOGL')
+	    .then(dailyData => {
+	        console.log("Daily data:");
+	        res.send(dailyData);// console.log(dailyData);
+	    })
+	    .catch(err => {
+	        console.error(err);
+	    });
 });
 
 
-app.listen(port, '0.0.0.0', function() {
+var server = app.listen(port, '0.0.0.0', function() {
   console.log(`Server running at http://localhost:${port}/`);
 });
